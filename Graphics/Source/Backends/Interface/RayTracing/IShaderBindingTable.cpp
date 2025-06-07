db3255ebfb321ff6be5516e2d0a9dc6dd37191fc
Murat Cengiz <cengizm9@gmail.com>
2025-06-07 15:17:23 +0200

    Minor linux build error

--- Graphics/Source/Backends/Interface/RayTracing/IShaderBindingTable.cpp
+++ Graphics/Source/Backends/Interface/RayTracing/IShaderBindingTable.cpp
@@ -18,6 +18,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "DenOfIzGraphics/Backends/Interface/RayTracing/IShaderBindingTable.h"
 #include "DenOfIzGraphicsInternal/Utilities/Logging.h"
+#include <sstream>
 
 using namespace DenOfIz;
 
