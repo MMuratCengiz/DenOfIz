ba427b7671257ccde64916df65c2356060c97fd8
Murat Cengiz <cengizm9@gmail.com>
2026-08-29 13:03:23 +0200

    Fix DxilToMsl compilation

--- Graphics/Source/Assets/Shaders/DxilToMsl.cpp
+++ Graphics/Source/Assets/Shaders/DxilToMsl.cpp
@@ -19,6 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "DenOfIzGraphicsInternal/Assets/Shaders/DxilToMsl.h"
 #include <functional>
 #include <ranges>
+#include <algorithm>
 #include <unordered_map>
 #include "DenOfIzGraphics/Assets/Shaders/ShaderCompiler.h"
 #include "DenOfIzGraphicsInternal/Assets/Shaders/DxcEnumConverter.h"
